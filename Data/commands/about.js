0e38bf88b3a26ce3d4ced8ad175ae3e124b5f6dc
Hacker Jeff <Hackerjef@users.noreply.github.com>
2018-01-27 21:38:54 -0500

    about page added :pineapple:
    ^^

---
+++ Data/commands/about.js
@@ -0,0 +1,14 @@
+exports.run = (console, Discord, client, message) => {
+  const embed = new Discord.RichEmbed()
+    .setTitle("A Discordbot Made by Nadie#0063")
+    .setAuthor("challangebot", client.user.avatarURL)
+    .setColor(5301186)
+    .setDescription("This bot is For zombie challanges~ And honestly this is the first bot that I ever made with sucess w/ the [source on github](https://github.com/Hackerjef/challangebot)")
+    .setURL("https://hackerjef.github.io")
+    .setThumbnail("hackerjef.github.io/Downloads/invisible.png")
+    .setFooter("© Weenie Bot by @Nadie#0063", client.user.avatarURL)
+    .addField("Command Prefix »", "?", true)
+    .addField("Commands »", "we have alot of commands bot well not documented, check for commands folder in the repo for a list :", true)
+    .setImage("hackerjef.github.io/Downloads/invisible.png");
+  message.channel.send({ embed });
+};
