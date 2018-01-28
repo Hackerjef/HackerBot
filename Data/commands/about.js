be0cb5789a60eb8e8e8d2dfb25b5bc5764c22bb0
Hacker Jeff <Hackerjef@users.noreply.github.com>
2018-01-27 21:40:46 -0500

    lol

--- Data/commands/about.js
+++ Data/commands/about.js
@@ -5,10 +5,10 @@ exports.run = (console, Discord, client, message) => {
     .setColor(5301186)
     .setDescription("This bot is For zombie challanges~ And honestly this is the first bot that I ever made with sucess w/ the [source on github](https://github.com/Hackerjef/challangebot)")
     .setURL("https://hackerjef.github.io")
-    .setThumbnail("hackerjef.github.io/Downloads/invisible.png")
+    .setThumbnail("https://hackerjef.github.io/Downloads/invisible.png")
     .setFooter("© Weenie Bot by @Nadie#0063", client.user.avatarURL)
     .addField("Command Prefix »", "?", true)
     .addField("Commands »", "we have alot of commands bot well not documented, check for commands folder in the repo for a list :", true)
-    .setImage("hackerjef.github.io/Downloads/invisible.png");
+    .setImage("https://hackerjef.github.io/Downloads/invisible.png");
   message.channel.send({ embed });
 };
